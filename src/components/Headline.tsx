b424322c02fc5f83c37ae02e4040735e62a2102a
adam-kb <4174755+adam-kb@users.noreply.github.com>
2025-05-21 16:05:21 -0600

    feat: Replace default content with Headline component in Home page
    Sets up the initial layout for the Headline section including responsive styling and design-specific padding. Marquee animation and GSAP interactions will be added in a future

---
+++ src/components/Headline.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: var(--font-size-h1);
+  letter-spacing: var(--letter-spacing-h1);
+  font-weight: var(--font-weight-h1);
+  font-style: var(--font-style-h1);
+  line-height: var(--line-height-h1);
+  text-align: center;
+  color:  var(--green-500);
+  padding: 0 var(--space-sm);
+
+  @media (min-width: 501px) {
+    line-height: 0.95;
+  }
+
+  @media (min-width: 1025px) {
+    padding: 0;
+  }
+`
+const Emphasis = styled.span`
+  color: var(--green-200);
+  font-style: italic;
+`
+
+const Headline = () => {
+  return (
+    <Title>Health insurance that <Emphasis>doesn&apos;t get in the way</Emphasis></Title>
+  )
+}
+
+export default Headline;
